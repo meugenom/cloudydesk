74427e8e111bbcf4c482fdc22b51f0c13943aef9
Eugen Morozov <meugenom@gmail.com>
2022-08-12 10:42:12 +0200

    rewrote drag and drop component

--- client/src/app/file/drag-drop-swap.directive.ts
+++ client/src/app/file/drag-drop-swap.directive.ts
@@ -6,12 +6,15 @@ import {
 	Input,
 	Output,
   } from '@angular/core';
+  import { Globals } from '../global';
   
   @Directive({
 	selector: '[appDragDropSwap]',
   })
   export class DragDropSwapDirective {
-	constructor() {}
+	constructor(
+		public globals: Globals
+	) {}
   
 	@HostBinding('attr.draggable') draggable = true;
 	@Input('elemPosition')
@@ -19,13 +22,46 @@ import {
 	@Input('list') list: any;
 	@Output('returnUpdatedList') returnUpdatedList = new EventEmitter<any>();
   
+	//need to know 
+	@HostListener('mousedown', ['$event'])
+	onMouseDown(e: any) {
+		
+		console.log('mouse down from drag element')
+		
+		//need stop this event but set selected for item
+		e.stopPropagation();
+		console.log(e.srcElement.attributes)
+		if(e.srcElement.attributes != undefined){
+			for (let index = 0; index < e.srcElement.attributes.length; index++) {
+				const element = e.srcElement.attributes[index];
+				if(element.name == 'data-dataitem'){
+					console.log(element.nodeValue)
+					let item = document.getElementById(element.nodeValue);
+					if(!item?.classList.contains('selected') && item!=null){
+						item?.classList.add('selected')
+						item.children[1].attributes[2].value = "background-color: #cdcdcd30; border-radius: 3px ; filter: drop-shadow(0 0 1px rgba(102, 102, 102, 1))"
+
+					}else if(item != null){
+						item?.classList.remove('selected')
+						item.children[1].attributes[2].value = " drop-shadow(1px 1px 1px rgba(102, 102, 102, 0.5))"
+					}
+
+					//this.globals.selection?.hasOwnProperty('start');
+					
+				}
+				
+			}
+		}
+	}
+
+
 	@HostListener('dragstart', ['$event'])
 	onDragStart(e: any) {
 
 		//need add transit class
 		const id  = e.srcElement.id
 		let elem = document.getElementById(id)
-		elem?.classList.add('item-transit')
+		//elem?.classList.add('item-transit')
 
 		//move parameter to cancel selection
 		elem?.setAttribute('dragstart','true');
@@ -50,7 +86,8 @@ import {
 	  for (let index = 0; index < elements.length; index++) {
 		const element = elements[index]
 		//console.log(element)
-		element?.classList.remove('item-transit')
+		//element?.classList.remove('item-transit')
+		
 		//set false to stop selection cancelation
 		element?.setAttribute('dragstart','false');
 	  }
