bf4c00b98b6660305f5f519d910788056282dc61
Eugen Morozov <meugenom@gmail.com>
2022-08-26 17:11:26 +0200

    added features auth server code

---
+++ server/README.md
@@ -0,0 +1,46 @@
+## Web-based desktop client (backend code)
+
+see **[Demo - https://neetcloud.dev](https://neetcloud.dev)**
+
+This project was generated with:
+
+- [Spring Boot](https://spring.io) version 2.6.2
+- [Spring MongoDB Starter](https://spring.io/guides/gs/accessing-data-mongodb/) version 2.6.2
+- JWT API (jjwt-api) version 0.11.2
+- [Lombok](https://projectlombok.org)
+
+! Important: 
+- Added CORS-configuration to working with frontend on http port 8081 for API calls. 
+- Need preinstalled MongoDb [mongodb-community version](https://www.mongodb.com/try/download/community), all settings are default.
+
+### **Features**
+- **API**:
+	- **Login** with response a new JWT-token
+	- **Registration** with creating a new user and return a new JWT-token
+	- ... in progress
+
+### Development server
+
+Run `mvn spring-boot:run` for a dev server. Navigate to `http://localhost:3000/`. The application will automatically reload if you change any of the source files.
+
+### Build
+
+Run `mvn clean` to build the project. The build artifacts will be stored in the `target/` directory.
+
+### Tests
+
+- JUnit Tests in progress;
+
+- API testing with `curl`:
+
+see example with testing `/auth/login`
+
+```
+curl -d '{"password":"tester","userName":"tester"}' -H 'Content-Type: application/json' http://localhost:3000/auth/login
+```
+
+in out:
+
+```
+{"error":false,"message":"Logged In","token":"eyJhbGciOiJIUzI1NiJ9 eyJzdWIiOiJ0ZXN0ZXIiLCJpYXQiOjE2NjE1ODkzNTIsImV4cCI6MTY2MTYwNzM1Mn0.lA9rIYmjfbw9G3cs2CWwew2JEJQrEtdlSPlc9BO0R_M"}%
+```
---
+++ server/src/main/java/dev/neetcloud/api/controller/AuthenticationController.java
@@ -0,0 +1,108 @@
+package dev.neetcloud.api.controller;
+
+import dev.neetcloud.api.model.User;
+import dev.neetcloud.api.repository.UserRepository;
+import dev.neetcloud.api.service.JwtUserDetailsService;
+import dev.neetcloud.api.util.JwtTokenUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	final UserRepository userRepository;
+	final AuthenticationManager authenticationManager;
+	final JwtUserDetailsService userDetailsService;
+	final JwtTokenUtil jwtTokenUtil;
+
+	public AuthenticationController(UserRepository userRepository, AuthenticationManager authenticationManager,
+			JwtUserDetailsService userDetailsService, JwtTokenUtil jwtTokenUtil) {
+		this.userRepository = userRepository;
+		this.authenticationManager = authenticationManager;
+		this.userDetailsService = userDetailsService;
+		this.jwtTokenUtil = jwtTokenUtil;
+	}
+
+	@PostMapping(value = "/login")
+	public ResponseEntity<Map<String, Object>> loginUser(
+			@RequestBody User user) {
+		Map<String, Object> responseMap = new HashMap<>();
+		try {
+
+			logger.info("password: " + user.getPassword());
+			logger.info("userName: " + user.getUserName());
+
+			Authentication auth = authenticationManager
+					.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+			if (auth.isAuthenticated()) {
+
+				logger.info("Logged In");
+
+				UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+				String token = jwtTokenUtil.generateToken(userDetails);
+				responseMap.put("error", false);
+				responseMap.put("message", "Logged In");
+				responseMap.put("token", token);
+				return ResponseEntity.ok(responseMap);
+			} else {
+				responseMap.put("error", true);
+				responseMap.put("message", "Invalid Credentials");
+				return ResponseEntity.status(401).body(responseMap);
+			}
+		} catch (DisabledException e) {
+			e.printStackTrace();
+			responseMap.put("error", true);
+			responseMap.put("message", "User is disabled");
+			return ResponseEntity.status(500).body(responseMap);
+		} catch (BadCredentialsException e) {
+			responseMap.put("error", true);
+			responseMap.put("message", "Invalid Credentials");
+			return ResponseEntity.status(401).body(responseMap);
+		} catch (Exception e) {
+			e.printStackTrace();
+			responseMap.put("error", true);
+			responseMap.put("message", "Something went wrong");
+			return ResponseEntity.status(500).body(responseMap);
+		}
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity<Map<String, Object>> saveUser(
+			@RequestBody User user) {
+
+		Map<String, Object> responseMap = new HashMap<>();
+
+		user.setFirstName(user.getFirstName());
+		user.setLastName(user.getLastName());
+		user.setEmail(user.getEmail());
+		user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+		user.setRole("USER");
+
+		user.setUserName(user.getUserName());
+		UserDetails userDetails = userDetailsService.createUserDetails(user.getUserName(), user.getPassword());
+		String token = jwtTokenUtil.generateToken(userDetails);
+		userRepository.save(user);
+		responseMap.put("error", false);
+		responseMap.put("username", user.getUserName());
+		responseMap.put("message", "Account created successfully");
+		responseMap.put("token", token);
+		return ResponseEntity.ok(responseMap);
+	}
+}
