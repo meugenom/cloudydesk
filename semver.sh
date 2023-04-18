8f49098eb50f45afcb93ff1cb14a49f6caae82aa
Eugen Morozov <meugenom@gmail.com>
2023-04-18 19:21:48 +0200

    Updated semver file to create new version

--- semver.sh
+++ semver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# (MIT) meugenom 2022
+# (MIT) meugenom 2023
 
 #############
 # [major].[minor].[patch]-[build/beta/rc]
@@ -7,98 +7,100 @@
 # last semver: $ git describe 
 #############
 
-#declare variables
-SEMVER_REGEX="^(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)(\\-[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?(\\+[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?$"
+#declare constants
+readonly SEMVER_REGEX="^(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)(\\-[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?(\\+[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?$"
 
 #get version
-version=`git describe`
+version=$(git describe)
 
 
 #get parts of version
 if [[ "$version" =~ $SEMVER_REGEX ]]; then
-	major=${BASH_REMATCH[1]}
-	minor=${BASH_REMATCH[2]}
-	patch=${BASH_REMATCH[3]}
-	prere=${BASH_REMATCH[4]}
-	build=${BASH_REMATCH[5]}
-	echo "Old version: " $major.$minor.$patch
+	old_major=${BASH_REMATCH[1]}
+	old_minor=${BASH_REMATCH[2]}
+	old_patch=${BASH_REMATCH[3]}
+	old_prerelease=${BASH_REMATCH[4]}
+	old_build=${BASH_REMATCH[5]}
+	echo "Old version: " $old_major.$old_minor.$old_patch
 fi
 
-old_version="$major.$minor.$patch"
+# get the first param from console (patch, minor, major)
+case "$1" in
+  patch)
+    echo "Old patch number is $old_patch"
+    new_patch=$((old_patch+1))
+    echo "New patch number is $new_patch"
+    new_version="$old_major.$old_minor.$new_patch"
+    ;;
+  minor)
+    echo "Old minor number is $old_minor"
+    new_minor=$((old_minor+1))
+    echo "New minor number is $new_minor"
+    new_patch=0
+    new_version="$old_major.$new_minor.$new_patch"
+    ;;
+  major)
+    echo "Old major number is $old_major"
+    new_major=$((old_major+1))
+    echo "New major number is $new_major"
+    new_patch=0
+    new_minor=0
+    new_version="$new_major.$new_minor.$new_patch"
+    ;;
+  *)
+    echo "Unknown parameter. Please use 'patch' or 'minor' or 'major'"
+    exit 1
+    ;;
+esac
 
-#get the first param from colnsole (patch, minor, major)
-str="$1"
-str_patch="patch"
-str_minor="minor"
-str_major="major"
- 
-if [ "$str" == "$str_patch" ]; then
-	echo "Old patch number is $patch"
-	patch=$((patch+1))
-	echo "New patch number is $patch"
-else
-	if [ "$str" == "$str_minor" ]; then
-		echo "Old minor number is $minor"
-		minor=$((minor+1))
-		echo "New minor number is $minor"
-		#base case
-		#need to write patch to '0', because version of the patch is 0 for new minor changes
-		patch="0"
-	else
-		if [ "$str" == "$str_major" ]; then
-			echo "Old major number is $major"
-			major=$((major+1))
-			echo "New major number is $major"
-			#base case
-			#need to write patch to '0' and minor to '0', because it's global change
-			patch="0"
-			minor="0"
-		else
-			echo "Unknown Parameter. Please use 'patch' or 'minor' or 'major'"
-			exit 1
-		fi
-	fi
-fi
 
-echo "New version is $major.$minor.$patch"
-new_version="$major.$minor.$patch"
+echo "New version is $new_version"
 
 #modify version number for client and server
-cd client
+cd client || exit 1
 file="package.json"
 
-if [ ! -f "${file}" ]; then
-	echo "file doesn't exist: ${file}"
+if [ ! -f "${package_file}" ]; then
+	echo "file doesn't exist: ${package_file}"
 	exit 1
 fi
 
 #BSD/OSX
-sed -i '' 's/"version": "'$old_version'"/"version": "'$new_version'"/' $file
+if ! sed -i '' "s/\"version\": \"$old_major.$old_minor.$old_patch\"/\"version\": \"$new_version\"/" "$package_file"; then
+  echo "Failed to modify $package_file"
+  exit 1
+fi
+
+cd ../server || exit 1
+pom_file="pom.xml"
+
+if [ ! -f "${pom_file}" ]; then
+	echo "File doesn't exist: $pom_file"
+	exit 1
+fi
+
+# BSD/OSX
+if ! sed -i '' "s/$old_version-SNAPSHOT/$new_version-SNAPSHOT/" "$pom_file"; then
+  echo "Failed to modify $pom_file"
+  exit 1
+fi
 
 cd ..
-cd server
-file="pom.xml"
+readme_file="README.md"
 
-if [ ! -f "${file}" ]; then
-	echo "file doesn't exist: ${file}"
+if [ ! -f "$readme_file" ]; then
+	echo "file doesn't exist: $readme_file"
 	exit 1
 fi
-#BSD/OSX
-sed -i '' 's/'$old_version'-SNAPSHOT/'$new_version'-SNAPSHOT/' $file
 
-cd ..
-file="README.md"
-if [ ! -f "${file}" ]; then
-	echo "file doesn't exist: ${file}"
-	exit 1
-fi
-#BSD/OSX
-sed -i '' 's/version-'$old_version'/version-'$major.$minor.$patch'/' $file
+# BSD/OSX
+sed -i '' "s/version-${old_version}/version-${new_version}/" "${readme_file}"
+
 
 #write new version as a tag
 # git tag -a "0.1.1" -m "version 0.1.1"
 
-git tag -a "$major.$minor.$patch" -m "version $major.$minor.$patch"
+git tag -a "${new_version}" -m "version ${new_version}"
 git describe
  
 #to delete old version 
