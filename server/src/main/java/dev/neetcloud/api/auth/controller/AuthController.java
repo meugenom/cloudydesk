4479c757f1fadc75200de8cd89c861e26f0fd527
Eugen Morozov <meugenom@gmail.com>
2023-05-08 15:03:53 +0200

    Updated /register rest call to return cookie and auth response

--- server/src/main/java/dev/neetcloud/api/auth/controller/AuthController.java
+++ server/src/main/java/dev/neetcloud/api/auth/controller/AuthController.java
@@ -1,27 +1,36 @@
 package dev.neetcloud.api.auth.controller;
 
 import dev.neetcloud.api.UserSecurity.dao.JpaUserDetailsService;
-import dev.neetcloud.api.UserSecurity.model.UserSecurity;
+
+import dev.neetcloud.api.auth.exception.NotFoundException;
 import dev.neetcloud.api.auth.request.AuthenticationRequest;
 import dev.neetcloud.api.auth.response.AuthenticationResponse;
 import dev.neetcloud.api.auth.service.AuthService;
+import dev.neetcloud.api.auth.utils.UtilsCookie;
 import dev.neetcloud.api.config.JwtUtils;
+import dev.neetcloud.api.users.model.Users;
 import dev.neetcloud.api.users.requests.UsersRequest;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.util.ArrayList;
 
 @RestController
@@ -29,59 +38,73 @@ import java.util.ArrayList;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
+	private final AuthenticationManager authenticationManager;
 
-    private final JpaUserDetailsService jpaUserDetailsService;
+	private final JpaUserDetailsService jpaUserDetailsService;
 
-    private final AuthService authService;
+	private final AuthService authService;
 
-    private final JwtUtils jwtUtils;
+	private final JwtUtils jwtUtils;
 
-    @PostMapping("/authenticate")
-    public ResponseEntity<String> authenticate(@RequestBody AuthenticationRequest request, HttpServletResponse response) {
-        try {
-            authenticationManager
-                    .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword(),
-                            new ArrayList<>()));
-            final UserDetails user = jpaUserDetailsService.loadUserByUsername(request.getEmail());
-            if (user != null) {
-                String jwt = jwtUtils.generateToken(user);
-                Cookie cookie = new Cookie("jwt", jwt);
-                cookie.setMaxAge(7 * 24 * 60 * 60); // expires in 7 days
-//                cookie.setSecure(true);
-                cookie.setHttpOnly(true);
-                cookie.setPath("/"); // Global
-                response.addCookie(cookie);
+	@PostMapping("/authenticate")
+	public ResponseEntity<String> authenticate(@RequestBody AuthenticationRequest request,
+			HttpServletResponse response) {
+		try {
+			authenticationManager
+					.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword(),
+							new ArrayList<>()));
+			final UserDetails user = jpaUserDetailsService.loadUserByUsername(request.getEmail());
+			if (user != null) {
+				String jwt = jwtUtils.generateToken(user);
+				response.addCookie(UtilsCookie.getCookie(jwt));
 
-				AuthenticationResponse authResponse = new AuthenticationResponse(request.getEmail(), jwt);				
+				AuthenticationResponse authResponse = new AuthenticationResponse(request.getEmail());
 				ObjectMapper objectMapper = new ObjectMapper();
 				String json = objectMapper.writeValueAsString(authResponse);
-                return ResponseEntity.ok().body(json);
-            }
-            return ResponseEntity.status(400).body("Error authenticating");
-        } catch (Exception e) {
-            System.out.println(e);
-            return ResponseEntity.status(400).body("" + e.getMessage());
-        }
-    }
+				return ResponseEntity.ok().body(json);
+			}
+			return ResponseEntity.status(400).body("Error authenticating");
+		} catch (Exception e) {
+			System.out.println(e);
+			return ResponseEntity.status(400).body("" + e.getMessage());
+		}
+	}
 
-    @PostMapping("/register")
-    public ResponseEntity<UserSecurity> register(@RequestBody UsersRequest user) throws Exception {
-	
-        return ResponseEntity.ok(authService.AddUser(user).map(UserSecurity::new).orElseThrow(() -> new Exception("Unknown")));
-    }
+	@PostMapping("/register")
+	public ResponseEntity<AuthenticationResponse> register(@RequestBody UsersRequest user, HttpServletResponse response)
+			throws Exception {
+		Users newUser;
+		try {
+			newUser = authService.AddUser(user)
+					.orElseThrow(() -> new NotFoundException("User not found"));
+
+		} catch (DuplicateKeyException e) {
+			response.addCookie(UtilsCookie.getCookie(null));
+			return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+		} catch (DataAccessException e) {
+			response.addCookie(UtilsCookie.getCookie(null));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		} catch (ConstraintViolationException e) {
+			response.addCookie(UtilsCookie.getCookie(null));
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+		}
+
+		String token = jwtUtils.generateToken(jpaUserDetailsService.loadUserByUsername(newUser.getEmail()));
+		response.addCookie(UtilsCookie.getCookie(token));
+
+		// Create AuthResponse object with token and user information
+		AuthenticationResponse authResponse = new AuthenticationResponse(newUser.getEmail());
+
+		return ResponseEntity.ok(authResponse);
+	}
 
 	@PostMapping("/logout")
 	public ResponseEntity<String> logout(HttpServletResponse response) {
-		//Delete user from context userDetailsService
+		// Delete user from context userDetailsService
 		SecurityContextHolder.getContext().setAuthentication(null);
 
 		// delete cookie jwt from client
-		Cookie cookie = new Cookie("jwt", null);
-		cookie.setMaxAge(0);
-		//cookie.setHttpOnly(true);
-		cookie.setPath("/"); // Global
-		response.addCookie(cookie);
+		response.addCookie(UtilsCookie.getCookie(null));
 		return ResponseEntity.ok().body("Logged out");
 	}
 
--- server/src/main/java/dev/neetcloud/api/auth/response/AuthenticationResponse.java
+++ server/src/main/java/dev/neetcloud/api/auth/response/AuthenticationResponse.java
@@ -1,22 +1,12 @@
 package dev.neetcloud.api.auth.response;
 
 public class AuthenticationResponse {
-    private String token;
     private String email;
 	private boolean isAuthenticated;
 
-    public AuthenticationResponse(String email, String token) {
-        this.token = token;
+    public AuthenticationResponse(String email) {
         this.email = email;
 		this.isAuthenticated = true;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
     }
 
     public String getEmail() {
