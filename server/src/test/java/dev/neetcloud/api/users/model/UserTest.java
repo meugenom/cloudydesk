5f677ed120eaf92c761f0834f73d689f6cdc57fe
Eugen Morozov <meugenom@gmail.com>
2023-05-14 17:16:45 +0200

    added testing for user model

---
+++ server/src/test/java/dev/neetcloud/api/users/model/UserTest.java
@@ -0,0 +1,21 @@
+package dev.neetcloud.api.users.model;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class UserTest {
+
+	//test create new
+	@Test
+	@DisplayName("Сreate new user with username and password")
+	public void testCreateNewUser() {
+		Users user = new Users("firstUserName", "lastUserName", "user@web.de", "password", "ROLE_USER", true);
+		assertTrue(user.getFirstName().equals("firstUserName"));
+		assertTrue(user.getLastName().equals("lastUserName"));
+		assertTrue(user.getEmail().equals("user@web.de"));
+		assertTrue(user.getPassword().equals("password"));
+		assertTrue(user.getRoles().equals("ROLE_USER"));
+		assertTrue(user.getIs_active().equals(true));
+	}
+}
