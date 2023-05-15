2cbf0ef37c43b15c327651085e29fe38b417dbf4
Eugen Morozov <meugenom@gmail.com>
2023-05-15 14:11:19 +0200

    add possibility to see self in the app camera

--- client/src/app/camera/camera.component.ts
+++ client/src/app/camera/camera.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, OnDestroy } from '@angular/core';
 
 @Component({
-  selector: 'app-camera',
-  templateUrl: './camera.component.html',
-  styleUrls: ['./camera.component.sass']
+	selector: 'app-camera',
+	templateUrl: './camera.component.html',
+	styleUrls: ['./camera.component.sass']
 })
 export class CameraComponent implements OnInit {
 
-  constructor() { }
+	@ViewChild('videoElement') videoElement: ElementRef | any;
+	videoWidth = 0;
+	videoHeight = 0;
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+
+	ngOnInit(): void {
+		this.startCamera();
+	}
+
+	ngOnDestroy(): void {
+		this.videoElement.nativeElement.srcObject.getTracks()[0].stop();
+	}
+
+	startCamera() {
+		if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+			navigator.mediaDevices.getUserMedia({ video: true })
+				.then(stream => {
+					this.videoElement.nativeElement.srcObject = stream;
+					this.videoElement.nativeElement.play();
+				})
+				.catch(error => console.error('Error accessing camera: ', error));
+		}
+	}
+
+	onVideoLoaded(metadata: any) {
+		this.videoWidth = metadata.target.videoWidth;
+		this.videoHeight = metadata.target.videoHeight;
+	}
 
 }
