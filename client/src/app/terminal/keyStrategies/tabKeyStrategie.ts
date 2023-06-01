ae1b2f532dc46a34abc3f5a44e8f3a5292671d4c
Eugen Morozov <meugenom@gmail.com>
2023-06-01 10:48:52 +0200

    small fixes

--- client/src/app/terminal/keyStrategies/tabKeyStrategie.ts
+++ client/src/app/terminal/keyStrategies/tabKeyStrategie.ts
@@ -5,8 +5,7 @@ export class TabKeyStrategy extends KeyStrategy {
 	execute(terminal: Terminal, e: KeyboardEvent) {
 		e.preventDefault();
 		//add 2 space
-		terminal._inputLine.textContent =
-		terminal._inputLine.textContent + `   `;
+		terminal._inputLine.textContent +=  `   `;
 		
 	}
   }
