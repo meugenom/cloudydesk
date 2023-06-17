773d45bc84311a5fb8164bf340258319ca8a1ae8
Eugen Morozov <meugenom@gmail.com>
2023-06-17 08:14:49 +0200

    added models, reducer for finder store values

---
+++ client/src/app/desktop/store/models/finder.state.model.ts
@@ -0,0 +1,5 @@
+import { Finder } from './finder.model';
+
+export interface FinderState {
+    environment: Finder
+}
---
+++ client/src/app/desktop/store/reducers/finder.reducer.ts
@@ -0,0 +1,19 @@
+// import the interface
+import { Finder } from '../models/finder.model';
+import { AddFinder } from '../actions/finder.action';
+import { createReducer, on } from '@ngrx/store';
+
+//create a dummy initial state
+const initialState: Finder =
+{
+    currentDir: '',
+};
+
+export const FinderReducer = createReducer(
+    initialState,
+    on(AddFinder, (state, action) => (
+        {
+            currentDir: action.currentDir
+        }
+    ))
+);
