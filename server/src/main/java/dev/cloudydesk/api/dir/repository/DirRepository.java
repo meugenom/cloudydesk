38c5f15af6a7e28785c0474d6cf91e390a446c82
Eugen Morozov <meugenom@gmail.com>
2023-08-17 14:07:44 +0200

    added Dir controller to backend
    added Dir controller with POST, PUT, DELETE dir
    added some calls with curl in the text file as example

--- server/src/main/java/dev/cloudydesk/api/dir/repository/DirRepository.java
+++ server/src/main/java/dev/cloudydesk/api/dir/repository/DirRepository.java
@@ -1,18 +1,17 @@
 package dev.cloudydesk.api.dir.repository;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import dev.cloudydesk.api.dir.model.Dir;
 
+
 @Repository
 public interface DirRepository extends JpaRepository<Dir, Long> {
 
-    // find dir by dirId
-    Optional findById(Long id);
+    // find dir by dirId    
+	Dir getOne(Long id);
 
     // find all dirs by created user id and name
     List<Dir> findByCreatedUserIdAndDirName(Long createdUserId, String name);
